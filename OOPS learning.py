22e99d00928cbc37eafa6993f11bef8a34134e61
KrishnaDass95 <krishdass95@gmail.com>
2023-01-16 12:51:41 +0530

    class and static methods

--- OOPS learning.py
+++ OOPS learning.py
@@ -30,14 +30,14 @@ class Item:
 
     # instead of creating objects from the python file, we can create objects using CSV
     # to do that, we can instantiate from the class, without creating instances outside the class
-    # we need to use a class method for it, using a decorater, which changes a functions behaviour
+    # we need to use a class method for it, using a decorater(@), which changes a functions behaviour
     @classmethod
     # the cls, class itself is passed as the first argument by default
     def instantiateFromCSV(cls):  # we aren't passing self cause we're not creating any objects that get passed
-        with open('items.csv', 'r') as f:
-            reader = csv.DictReader(f)
-            items = list(reader)
-        for item in items:
+        with open('items.csv', 'r') as f: # context manager to read the file, open items.csv in read mode
+            reader = csv.DictReader(f) # convert the csv into a python dictonary
+            items = list(reader) # put that/ convert the dict into a list
+        for item in items: # objects initialise
             Item(
                 name=item.get('name'),
                 price=float(item.get('price')),
@@ -49,8 +49,9 @@ class Item:
 
     @staticmethod # we never send objects as argument to static method
     def is_integer(num): # static method is like isolated function
+        # we will remove the floats that have point 0
+        # For i.e: 5.0 -> 5,
         if isinstance(num, float):
-            # remove the decimal point
             return num.is_integer()
         elif isinstance(num, int):
             return True
@@ -59,15 +60,14 @@ class Item:
 
     # repr -> represents your objects, it helps you to control how the object is displayed when printed
     def __repr__(self):  # magic method to make good sense of the instances when printed
-        return f"Item(`{self.name}`, {self.price}, {self.quantity})"
+        return f"Item('{self.name}', {self.price}, {self.quantity})"
 
 
 Item.instantiateFromCSV()
-print(Item.all)
 
 # when do you use static methods? It's like a standlone function outside a class,
 # it has nothing to do with an instance. But maybe somewhat related to a class so use a static method
-
+print(Item.is_integer(7.0))
 
 # class method is used to instantiate an instance from a structured data
 # static methods do not pass any arguments like self or cls for it.
